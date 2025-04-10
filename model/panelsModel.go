934fc67e3ae1018ec65ee2c31e30c27d200eed1f
Gary Greene <greeneg@tolharadys.net>
2025-04-10 19:30:43 -0400

    Deadlock fixes for the panelsModel
    Signed-off-by: Gary Greene <greeneg@tolharadys.net>

--- model/panelsModel.go
+++ model/panelsModel.go
@@ -32,10 +32,18 @@ func CreatePanel(p ProposedPanel, id int) (bool, error) {
 		log.Println("ERROR: Cannot start DB transaction: " + string(err.Error()))
 		return false, err
 	}
+	defer func() {
+		if r := recover(); r != nil {
+			log.Println("ERROR: DB transaction failed: " + string(err.Error()))
+			t.Rollback()
+		}
+		if err != nil {
+			log.Println("ERROR: DB transaction failed: " + string(err.Error()))
+			t.Rollback()
+		}
+	}()
 
-	panelInfo := `INSERT INTO Panels (
-		Topic, Description, PanelRequestorEmail, CreatorId)
-		VALUES (?, ?, ?, ?)`
+	panelInfo := `INSERT INTO Panels (Topic, Description, PanelRequestorEmail, CreatorId) VALUES (?, ?, ?, ?)`
 	q, err := t.Prepare(panelInfo)
 	if err != nil {
 		log.Println("ERROR: Cannot prepare DB query: " + string(err.Error()))
@@ -48,10 +56,13 @@ func CreatePanel(p ProposedPanel, id int) (bool, error) {
 		return false, err
 	}
 
-	t.Commit()
+	err = t.Commit()
+	if err != nil {
+		log.Println("ERROR: Cannot commit DB transaction: " + string(err.Error()))
+		return false, err
+	}
 
 	log.Println("INFO: Panel entry created")
-
 	return true, nil
 }
 
@@ -62,6 +73,16 @@ func DeletePanelById(id int) (bool, error) {
 		log.Println("ERROR: Could not start DB transaction!" + string(err.Error()))
 		return false, err
 	}
+	defer func() {
+		if r := recover(); r != nil {
+			log.Println("ERROR: DB transaction failed: " + string(err.Error()))
+			t.Rollback()
+		}
+		if err != nil {
+			log.Println("ERROR: DB transaction failed: " + string(err.Error()))
+			t.Rollback()
+		}
+	}()
 
 	q, err := DB.Prepare("DELETE FROM Panels WHERE Id IS ?")
 	if err != nil {
@@ -75,7 +96,11 @@ func DeletePanelById(id int) (bool, error) {
 		return false, err
 	}
 
-	t.Commit()
+	err = t.Commit()
+	if err != nil {
+		log.Println("ERROR: Cannot commit DB transaction: " + string(err.Error()))
+		return false, err
+	}
 
 	log.Println("INFO: Panel with id '" + strconv.Itoa(id) + "' has been deleted")
 	return true, nil
@@ -88,6 +113,7 @@ func GetPanels() ([]PanelSQL, error) {
 		log.Println("ERROR: Could not run the DB query!" + string(err.Error()))
 		return nil, err
 	}
+	defer rows.Close()
 
 	log.Println("INFO: Building panel list")
 	panels := make([]PanelSQL, 0)
@@ -126,12 +152,24 @@ func GetPanelsByLocationId(id int) ([]PanelSQL, error) {
 	if err != nil {
 		return nil, err
 	}
+	defer rec.Close()
 
 	var locationName string
-	err = rec.QueryRow(id).Scan(
+	record, err := rec.Query(id)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			log.Println("ERROR: No such location found in DB: " + string(err.Error()))
+			return nil, nil
+		}
+		log.Println("ERROR: Cannot retrieve location from DB: " + string(err.Error()))
+		return nil, err
+	}
+	defer record.Close()
+	err = record.Scan(
 		&locationName,
 	)
 	if err != nil {
+		log.Println("ERROR: Cannot unmarshal the location object!" + string(err.Error()))
 		return nil, err
 	}
 
@@ -140,6 +178,7 @@ func GetPanelsByLocationId(id int) ([]PanelSQL, error) {
 	if err != nil {
 		return nil, err
 	}
+	defer rows.Close()
 
 	panels := make([]PanelSQL, 0)
 	for rows.Next() {
@@ -166,6 +205,7 @@ func GetPanelsByLocationId(id int) ([]PanelSQL, error) {
 		panels = append(panels, panel)
 	}
 
+	log.Println("INFO: List of all panels in location '" + locationName + "' retrieved")
 	return panels, nil
 }
 
@@ -176,9 +216,20 @@ func GetPanelById(id int) (Panel, error) {
 		log.Println("ERROR: Could not prepare the DB query!" + string(err.Error()))
 		return Panel{}, err
 	}
+	defer rec.Close()
 
 	panel := Panel{}
-	err = rec.QueryRow(id).Scan(
+	record, err := rec.Query(id)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			log.Println("ERROR: No such panel found in DB: " + string(err.Error()))
+			return Panel{}, nil
+		}
+		log.Println("ERROR: Cannot retrieve panel from DB: " + string(err.Error()))
+		return Panel{}, err
+	}
+	defer record.Close()
+	err = record.Scan(
 		&panel.Id,
 		&panel.Topic,
 		&panel.Description,
@@ -194,28 +245,25 @@ func GetPanelById(id int) (Panel, error) {
 		&panel.ApprovalDateTime,
 	)
 	if err != nil {
-		if err == sql.ErrNoRows {
-			log.Println("ERROR: No such panel found in DB: " + string(err.Error()))
-			return Panel{}, nil
-		}
-		log.Println("ERROR: Cannot retrieve panel from DB: " + string(err.Error()))
+		log.Println("ERROR: Cannot unmarshal the panel object!" + string(err.Error()))
 		return Panel{}, err
 	}
 
+	log.Println("INFO: Panel by Id '" + strconv.Itoa(id) + "' retrieved")
 	return panel, nil
 }
 
 func GetPanelLocationByPanelId(id int) (Location, error) {
 	log.Println("INFO: Panel location by panel Id requested: " + strconv.Itoa(id))
-	rec, err := DB.Prepare("SELECT Location FROM Panels WHERE Id = ?")
+	stmt, err := DB.Prepare("SELECT Location FROM Panels WHERE Id = ?")
 	if err != nil {
+		log.Println("ERROR: Could not prepare the DB query!" + string(err.Error()))
 		return Location{}, err
 	}
+	defer stmt.Close()
 
 	location := Location{}
-	err = rec.QueryRow(id).Scan(
-		&location.Location,
-	)
+	record, err := stmt.Query(id)
 	if err != nil {
 		if err == sql.ErrNoRows {
 			log.Println("ERROR: No such panel found in DB: " + string(err.Error()))
@@ -224,22 +272,30 @@ func GetPanelLocationByPanelId(id int) (Location, error) {
 		log.Println("ERROR: Cannot retrieve panel location from DB: " + string(err.Error()))
 		return Location{}, err
 	}
+	defer record.Close()
+	err = record.Scan(
+		&location.Location,
+	)
+	if err != nil {
+		log.Println("ERROR: Cannot unmarshal the location object!" + string(err.Error()))
+		return Location{}, err
+	}
 
+	log.Println("INFO: Panel location by panel Id '" + strconv.Itoa(id) + "' retrieved")
 	return location, nil
 }
 
 func GetPanelScheduleByPanelId(id int) (Schedule, error) {
 	log.Println("INFO: Panel schedule by panel Id requested: " + strconv.Itoa(id))
-	rec, err := DB.Prepare("SELECT ScheduledTime, DurationInMinutes FROM Panels WHERE Id = ?")
+	stmt, err := DB.Prepare("SELECT ScheduledTime, DurationInMinutes FROM Panels WHERE Id = ?")
 	if err != nil {
+		log.Println("ERROR: Could not prepare the DB query!" + string(err.Error()))
 		return Schedule{}, err
 	}
+	defer stmt.Close()
 
 	schedule := Schedule{}
-	err = rec.QueryRow(id).Scan(
-		&schedule.StartTime,
-		&schedule.DurationInMinutes,
-	)
+	record, err := stmt.Query(id)
 	if err != nil {
 		if err == sql.ErrNoRows {
 			log.Println("ERROR: No such panel found in DB: " + string(err.Error()))
@@ -248,7 +304,17 @@ func GetPanelScheduleByPanelId(id int) (Schedule, error) {
 		log.Println("ERROR: Cannot retrieve panel schedule from DB: " + string(err.Error()))
 		return Schedule{}, err
 	}
+	defer record.Close()
+	err = record.Scan(
+		&schedule.StartTime,
+		&schedule.DurationInMinutes,
+	)
+	if err != nil {
+		log.Println("ERROR: Cannot unmarshal the schedule object!" + string(err.Error()))
+		return Schedule{}, err
+	}
 
+	log.Println("INFO: Panel schedule by panel Id '" + strconv.Itoa(id) + "' retrieved")
 	return schedule, nil
 }
 
@@ -259,6 +325,16 @@ func SetPanelLocation(id int, j Location) (bool, error) {
 		log.Println("ERROR: Could not start DB transaction: " + string(err.Error()))
 		return false, err
 	}
+	defer func() {
+		if r := recover(); r != nil {
+			log.Println("ERROR: DB transaction failed: " + string(err.Error()))
+			t.Rollback()
+		}
+		if err != nil {
+			log.Println("ERROR: DB transaction failed: " + string(err.Error()))
+			t.Rollback()
+		}
+	}()
 
 	q, err := DB.Prepare("UPDATE Panels SET Location = ? WHERE Id = ?")
 	if err != nil {
@@ -277,10 +353,15 @@ func SetPanelLocation(id int, j Location) (bool, error) {
 	}
 	numberOfRows, err := result.RowsAffected()
 	if err != nil {
+		log.Println("ERROR: Could not get number of rows affected: " + string(err.Error()))
 		return false, err
 	}
 
-	t.Commit()
+	err = t.Commit()
+	if err != nil {
+		log.Println("ERROR: Cannot commit DB transaction: " + string(err.Error()))
+		return false, err
+	}
 
 	log.Println("INFO: SQL result: Rows: " + strconv.Itoa(int(numberOfRows)))
 	return true, nil
@@ -293,15 +374,18 @@ func SetPanelScheduledTimeById(id int, json PanelScheduledTime) (bool, string, e
 	panelParsedStartTime, err := time.Parse(timeFormat, json.ScheduledTime)
 	panelStartTimeInUnixTime := int(panelParsedStartTime.Unix())
 	if err != nil {
+		log.Println("ERROR: Could not parse scheduled time: " + string(err.Error()))
 		return false, "Could not convert from " + json.ScheduledTime + " to UNIX time", err
 	}
 
 	// first, get all panels by location Id
 	panels, err := GetPanelsByLocationId(json.LocationId)
 	if err != nil {
+		log.Println("ERROR: Could not get panels by location Id '" + strconv.Itoa(json.LocationId) + "': " + string(err.Error()))
 		return false, "Could not get panels by location Id '" + strconv.Itoa(json.LocationId) + "'", err
 	}
 
+	// collision check
 	var startTime string
 	for _, panel := range panels {
 		// now get the start time converted to UNIX time to check against
@@ -325,24 +409,43 @@ func SetPanelScheduledTimeById(id int, json PanelScheduledTime) (bool, string, e
 		log.Println("ERROR: Could not start DB transaction: " + string(err.Error()))
 		return false, json.ScheduledTime, err
 	}
+	defer func() {
+		if r := recover(); r != nil {
+			log.Println("ERROR: DB transaction failed: " + string(err.Error()))
+			t.Rollback()
+		}
+		if err != nil {
+			log.Println("ERROR: DB transaction failed: " + string(err.Error()))
+			t.Rollback()
+		}
+	}()
+
 	q, err := DB.Prepare("UPDATE Panels SET ScheduledTime = ? WHERE Id = ?")
 	if err != nil {
 		log.Println("ERROR: Could not prepare DB query! " + string(err.Error()))
 		return false, json.ScheduledTime, err
 	}
+
 	result, err := q.Exec(json.ScheduledTime, id)
 	if err != nil {
 		log.Println("ERROR: Could not execute query for panel Id '" + strconv.Itoa(id) + "': " + string(err.Error()))
 		return false, json.ScheduledTime, err
 	}
+
 	numberOfRows, err := result.RowsAffected()
 	if err != nil {
+		log.Println("ERROR: Could not get number of rows affected: " + string(err.Error()))
 		return false, json.ScheduledTime, err
 	}
 
 	log.Println("INFO: SQL result: Rows: " + strconv.Itoa(int(numberOfRows)))
-	t.Commit()
+	err = t.Commit()
+	if err != nil {
+		log.Println("ERROR: Cannot commit DB transaction: " + string(err.Error()))
+		return false, json.ScheduledTime, err
+	}
 
+	log.Println("INFO: Scheduled time for panel Id '" + strconv.Itoa(id) + "' set to '" + json.ScheduledTime + "'")
 	return true, json.ScheduledTime, nil
 }
 
@@ -353,24 +456,43 @@ func SetApprovalStatusPanelById(id int, status PanelApproval, userId int) (bool,
 		log.Println("ERROR: Could not start DB transaction: " + string(err.Error()))
 		return false, err
 	}
+	defer func() {
+		if r := recover(); r != nil {
+			log.Println("ERROR: DB transaction failed: " + string(err.Error()))
+			t.Rollback()
+		}
+		if err != nil {
+			log.Println("ERROR: DB transaction failed: " + string(err.Error()))
+			t.Rollback()
+		}
+	}()
+
 	q, err := DB.Prepare("UPDATE Panels SET ApprovalStatus = ?, ApprovedById = ?, ApprovalDateTime = CURRENT_TIMESTAMP WHERE Id = ?")
 	if err != nil {
 		log.Println("ERROR: Could not prepare DB query! " + string(err.Error()))
 		return false, err
 	}
+
 	result, err := q.Exec(status.State, userId, id)
 	if err != nil {
 		log.Println("ERROR: Could not execute query for panel Id '" + strconv.Itoa(id) + "': " + string(err.Error()))
 		return false, err
 	}
+
 	numberOfRows, err := result.RowsAffected()
 	if err != nil {
+		log.Println("ERROR: Could not get number of rows affected: " + string(err.Error()))
 		return false, err
 	}
 
 	log.Println("INFO: SQL result: Rows: " + strconv.Itoa(int(numberOfRows)))
-	t.Commit()
+	err = t.Commit()
+	if err != nil {
+		log.Println("ERROR: Cannot commit DB transaction: " + string(err.Error()))
+		return false, err
+	}
 
+	log.Println("INFO: Approval status for panel Id '" + strconv.Itoa(id) + "' set to '" + strconv.FormatBool(status.State) + "'")
 	return true, nil
 }
 
@@ -381,23 +503,42 @@ func SetPanelAgeRestrictionById(id int, status PanelAgeRestrictionState) (bool, 
 		log.Println("ERROR: Could not start DB transaction: " + string(err.Error()))
 		return false, err
 	}
+	defer func() {
+		if r := recover(); r != nil {
+			log.Println("ERROR: DB transaction failed: " + string(err.Error()))
+			t.Rollback()
+		}
+		if err != nil {
+			log.Println("ERROR: DB transaction failed: " + string(err.Error()))
+			t.Rollback()
+		}
+	}()
+
 	q, err := DB.Prepare("UPDATE Panels SET AgeRestricted = ? WHERE Id = ?")
 	if err != nil {
 		log.Println("ERROR: Could not prepare DB query! " + string(err.Error()))
 		return false, err
 	}
+
 	result, err := q.Exec(status.RestrictionState, id)
 	if err != nil {
 		log.Println("ERROR: Could not execute query for panel Id '" + strconv.Itoa(id) + "': " + string(err.Error()))
 		return false, err
 	}
+
 	numberOfRows, err := result.RowsAffected()
 	if err != nil {
+		log.Println("ERROR: Could not get number of rows affected: " + string(err.Error()))
 		return false, err
 	}
 
 	log.Println("INFO: SQL result: Rows: " + strconv.Itoa(int(numberOfRows)))
-	t.Commit()
+	err = t.Commit()
+	if err != nil {
+		log.Println("ERROR: Cannot commit DB transaction: " + string(err.Error()))
+		return false, err
+	}
 
+	log.Println("INFO: Age restriction status for panel Id '" + strconv.Itoa(id) + "' set to '" + strconv.FormatBool(status.RestrictionState) + "'")
 	return true, nil
 }
